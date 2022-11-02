c74c34c21741e1374f14ff7ab1b9a895359ffa62
Emmanuel Dada <Emmanuel.dada71@gmail.com>
2022-11-02 12:19:52 -0500

    initial commit

---
+++ arrays.js
@@ -0,0 +1,24 @@
+/*
+    In this file, we'll be continuing our story
+    with Jon being allowed to flee his fight with Jamie.
+*/
+
+
+let jonSnowAttack = 25;
+let jamieLanniesterAttack = 35;
+
+if(jonSnowAttack > jamieLanniesterAttack)
+{
+    console.log("Jon Snow has a  better attack than Jamie Lannister");
+}
+else if (jamieLanniesterAttack > jonSnowAttack)
+{
+    console.log("Jamie Lannister has a better attack than Jon Snow");
+} 
+else
+{
+    console.log("It's a tie");
+}
+
+
+
---
+++ varsAndConditionals.js
@@ -0,0 +1,43 @@
+/*
+    In this file, we'll be using variables, conditionals,
+    and loops to play out a fight between Jon Snow and
+    Jamie Lannister.
+*/ 
+
+let jonSnowAttack = 25;
+let jamieLanniesterAttack = 35;
+
+if(jonSnowAttack > jamieLanniesterAttack)
+{
+    console.log("Jon Snow has a  better attack than Jamie Lannister");
+}
+else if (jamieLanniesterAttack > jonSnowAttack)
+{
+    console.log("Jamie Lannister has a better attack than Jon Snow");
+} 
+else
+{
+    console.log("It's a tie");
+}
+
+
+let jonSnowHealth = 100;
+let jonSnowDefense = 0;
+
+if(jonSnowHealth < jamieLanniesterAttack)
+{
+    console.log("Jon Snow has been slain!!!");
+}else
+{
+    jonSnowHealth = jonSnowHealth - jamieLanniesterAttack;
+    console.log(`Jon Snow's health is down to ${jonSnowHealth}`);
+}
+
+let healthkit = 50;
+
+if(jonSnowHealth > 50)
+{
+    jonSnowHealth = 100;
+}else{
+    jonSnowHealth += 50;
+}
